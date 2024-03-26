2fdc27c718a3ca8084dbee62da416ee876e4bab5
Mahboob-A <iammahboob.a@gmail.com>
2024-03-26 19:17:22 +0530

    response with body data

--- app/main.py
+++ app/main.py
@@ -10,15 +10,24 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     print('server started...')
+    
+    OK_200 = 'HTTP/1.1 200 OK\r\n'
+    NOT_FOUND_404 = 'HTTP/1.1 404 Not Found\r\n'
+    END_HEADER = '\r\n'
+    
     while True: 
         try: 
             conn, addr = server_socket.accept() 
             with conn: 
                 print('Server connected to {}:{}'.format(addr[0], addr[1]))
                 data = conn.recv(1024).decode('utf-8')
+                print(data)
                 data = data.split('\r\n')
+                print(data)
                 data = data[0].split(' ')
+                print(data)
                 path = data[1]
+                print(path)
                 # data.split('\r\n')
                 # ['GET /index.html HTTP/1.1', 'Host: localhost:4221', 'User-Agent: curl/7.81.0', 'Accept: */*', '', '']
 
@@ -30,6 +39,11 @@ def main():
 
                 if path == '/': 
                     response = "HTTP/1.1 200 OK\r\n\r\n".encode("utf-8")
+                elif path.startswith('/echo/'):
+                    body_data = path[6:]
+                    CONTENT_TYPE = 'Content-Type: text/plain\r\n'
+                    CONTENT_LENGTH = f'Content-Length: {len(body_data)}\r\n'
+                    response = (OK_200 +  CONTENT_TYPE + CONTENT_LENGTH + END_HEADER + body_data).encode('utf-8')
                 else: 
                     response = "HTTP/1.1 404 Not Found\r\n\r\n".encode("utf-8")
                 conn.send(response)
