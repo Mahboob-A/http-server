f77ef579e10221d4e81674664b3af6854692e491
Mahboob-A <iammahboob.a@gmail.com>
2024-03-26 18:53:27 +0530

    parse start line from request body

--- app/main.py
+++ app/main.py
@@ -9,18 +9,34 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    print('server started...')
     while True: 
         try: 
             conn, addr = server_socket.accept() 
             with conn: 
                 print('Server connected to {}:{}'.format(addr[0], addr[1]))
                 data = conn.recv(1024).decode('utf-8')
-                conn.send('HTTP/1.1 200 OK\r\n\r\n'.encode('utf-8'))
+                data = data.split('\r\n')
+                data = data[0].split(' ')
+                path = data[1]
+                # data.split('\r\n')
+                # ['GET /index.html HTTP/1.1', 'Host: localhost:4221', 'User-Agent: curl/7.81.0', 'Accept: */*', '', '']
+
+                # data[0].split(' ')
+                # GET /index.html HTTP/1.1
+
+                # data[1] # /index.html
+                # ['GET', '/index.html', 'HTTP/1.1']
+
+                if path == '/': 
+                    response = "HTTP/1.1 200 OK\r\n\r\n".encode("utf-8")
+                else: 
+                    response = "HTTP/1.1 404 Not Found\r\n\r\n".encode("utf-8")
+                conn.send(response)
         except: 
             print('Server closed!')
             server_socket.close()
             break
-    
 
 
 if __name__ == "__main__":
