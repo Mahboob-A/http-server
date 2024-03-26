aa8d229751f27940c9d5533d8764c82328d4f456
Mahboob-A <iammahboob.a@gmail.com>
2024-03-26 23:02:50 +0530

    concurrent requests

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ import threading
 import sys 
 
 
-
-
 OK_200 = b'HTTP/1.1 200 OK\r\n'
 NOT_FOUND_404 = b'HTTP/1.1 404 Not Found\r\n'
 END_HEADER = b'\r\n'
@@ -18,27 +16,29 @@ def handle_connections(conn):
     with conn: 
         # all data in bytes
         data = conn.recv(1024)
-        headers = data.split(b'\r\n')
-        path = headers[0].split()[1]
-        user_agent = headers[2].split()[1]  # user_agent:  b'curl/7.81.0'
+        if data: 
+            headers = data.split(b'\r\n')
+            path = headers[0].split()[1]
+            user_agent = headers[2].split()[1]  # user_agent:  b'curl/7.81.0'
 
-        print('headers: ', headers)
-        print('path: ', path)
-        print('user_agent: ', user_agent)
+            print('headers: ', headers)
+            print('path: ', path)
+            print('user_agent: ', user_agent)
 
-        if path == b'/': 
-            response = OK_200 + END_HEADER
-        elif path.startswith(b'/echo/'):
-            body_data = path[6:] # already in bytes 
-            CONTENT_LENGTH = get_content_length(body_data)
-            response = OK_200 +  CONTENT_TYPE + CONTENT_LENGTH + END_HEADER + body_data
-        elif path == b'/user-agent': 
-            CONTENT_LENGTH = get_content_length(user_agent)
-            response = OK_200 + CONTENT_TYPE + CONTENT_LENGTH + END_HEADER + user_agent
+            if path == b'/': 
+                response = OK_200 + END_HEADER
+            elif path.startswith(b'/echo/'):
+                body_data = path[6:] # already in bytes 
+                CONTENT_LENGTH = get_content_length(body_data)
+                response = OK_200 +  CONTENT_TYPE + CONTENT_LENGTH + END_HEADER + body_data
+            elif path == b'/user-agent': 
+                CONTENT_LENGTH = get_content_length(user_agent)
+                response = OK_200 + CONTENT_TYPE + CONTENT_LENGTH + END_HEADER + user_agent
+            else: 
+                response = NOT_FOUND_404 + END_HEADER 
         else: 
-            response = NOT_FOUND_404 + END_HEADER 
-            
-        # response 
+            response = NOT_FOUND_404 + END_HEADER
+        # response
         conn.send(response)    
 
 
@@ -52,10 +52,9 @@ def main():
             conn, addr = socket_server.accept()
             print('Connected to: {}:{}'.format(addr[0], addr[1]))
             worker = threading.Thread(target=handle_connections, args=(conn, ))
-            worker.daemon = True 
+            # worker.daemon = True 
             worker.start()
 
 
 if __name__ == "__main__":
     main()
-            
