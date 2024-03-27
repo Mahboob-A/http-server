e0d52a8ea90986c56c41c8a31df0638f59be46dd
Mahboob-A <iammahboob.a@gmail.com>
2024-03-27 10:39:02 +0530

    complete: save data in POST request

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ import threading
 import sys
 import os
 import argparse
-import mimetypes
 
 OK_200 = "HTTP/1.1 200 OK\r\n"
 NOT_FOUND_404 = "HTTP/1.1 404 Not Found\r\n"
@@ -27,15 +26,14 @@ def handle_connections(conn, directory):
     path = headers[1]
     request = headers[0]
 
-    print("headers: ", headers)
-    print("path: ", path)
+    # print("headers: ", headers)
+    # print("path: ", path)
 
     if path == "/":
         response = OK_200 + END_HEADER
         conn.send(response.encode("utf-8"))
     elif "/echo/" in path: 
         body_data = path[6:]  
-        print(body_data)
         CONTENT_LENGTH = get_content_length(body_data)
         response = (
             OK_200 + CONTENT_TYPE + CONTENT_LENGTH + END_HEADER + body_data + BODY_END_HEADER
@@ -43,33 +41,24 @@ def handle_connections(conn, directory):
         conn.send(response.encode('utf-8'))
     elif "/user-agent" in path:
         user_agent = headers[6]  # user_agent:  b'curl/7.81.0'
-        print("user_agent: ", user_agent)
         CONTENT_LENGTH = get_content_length(user_agent)
         response = (
             OK_200 + CONTENT_TYPE + CONTENT_LENGTH + END_HEADER + user_agent + BODY_END_HEADER
         )
         conn.send(response.encode("utf-8"))
     elif "/files/" in path and request == 'GET':  
-        print('path in elif files: ', path)
-        print('actual dir name: ', path[7:])
-        print('dir in elif: ', directory)
         file_path = os.path.join(directory, path[7:])
-        print('curr-dir: ', os.getcwd())
-        print('file path: ', file_path)
         if os.path.exists(file_path):
             print('true ===')
             try:
                 with open(file_path, "rb") as f:
                     file_contents = f.read()
-                print('file-contents: ', file_contents)
-                print('200 OK')    
                 conn.send(
                     f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(file_contents)}\r\n\r\n".encode()
                     + file_contents
                 )
             except Exception as e:
                 print("Error:", e)
-                print('404 NOT FOUND')
                 response = NOT_FOUND_404 + END_HEADER
                 conn.send(response.encode("utf-8"))
         else:
@@ -78,18 +67,11 @@ def handle_connections(conn, directory):
     elif 'files' in path and request == 'POST': 
         filename = path[7:]
         file_path = os.path.join(directory, filename)
-        print(raw_data.split(b"\r\n\r\n", 1)[1])
-        a = raw_data.split(b"\r\n\r\n", 1)
-        print('a: ', a)
-        print(len(a))
-        b = raw_data.split(b"\r\n\r\n")[1]
-        print('b: ', b)
-        print(len(b))
-        c = raw_data.split(b"\r\n\r\n",)
-        print('c: ', c)
-        print(len(c))
         with open(file_path, "wb") as f:
-            f.write(raw_data.split(b"\r\n\r\n")[1])
+            """
+            The 1 argument passed to split() indicates that the split should be performed at most once, resulting in a list with a maximum of two elements. The first element will contain everything before the first occurrence of b"//r/n/r/n, and the second element will contain everything after the first occurrence of b"\r\n\r\n", including subsequent occurrences if present.
+            """
+            f.write(raw_data.split(b"\r\n\r\n", 1)[1])  
         conn.send(OK_201.encode("utf-8"))
     else:
         response = NOT_FOUND_404 + END_HEADER
@@ -106,20 +88,19 @@ def main():
     parser.add_argument("--directory", type=str, default=os.getcwd())
     args = parser.parse_args()
     directory = args.directory 
-    print('directory: ', directory)
-    # try:
-    socket_server = socket.create_server((HOST, PORT), reuse_port=False)
-    while True:
-        conn, addr = socket_server.accept()
-        print("Connected to: {}:{}".format(addr[0], addr[1]))
-        worker = threading.Thread(target=handle_connections, args=(conn, directory))
-        worker.start()
-    # except KeyboardInterrupt:
-    #     print("Server is shutting down...")
-    #     sys.exit(0)
-    # except Exception as e:
-    #     print("An error occurred:", e)
-    #     sys.exit(1)
+    try:
+        socket_server = socket.create_server((HOST, PORT), reuse_port=False)
+        while True:
+            conn, addr = socket_server.accept()
+            print("Connected to: {}:{}".format(addr[0], addr[1]))
+            worker = threading.Thread(target=handle_connections, args=(conn, directory))
+            worker.start()
+    except KeyboardInterrupt:
+        print("Server is shutting down...")
+        sys.exit(0)
+    except Exception as e:
+        print("An error occurred:", e)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
